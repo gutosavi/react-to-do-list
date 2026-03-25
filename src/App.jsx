a2458d9bb41387df11bef90384097442ddc714b6
Gustavo Savi <gutosavi@hotmail.com>
2026-03-25 20:09:13 -0300

    style: setting main css config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import './index.css';
+import './App.css';
 
 function App() {
-  return <div>Teste</div>;
+  return (
+    <>
+      <header>...</header>
+      <main className="container">
+        <div className="task-box">
+          <div>
+            <input type="text" id="task" placeholder="Write your task here" />
+          </div>
+          <button className="btnSubmit">Enviar</button>
+          <ul>
+            <li>Tarefa 1</li>
+            <li>Tarefa 2</li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
